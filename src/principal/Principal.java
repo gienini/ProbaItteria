3a8e7b2d2a610ca03ab7e3190343cf78ae5b557a
gienini <Alumno22@SBS-ALUMNO22>
2014-02-11 09:35:11 +0000

    Afegit paquet principal, modificat gitignore

---
+++ src/principal/Principal.java
@@ -0,0 +1,13 @@
+package principal;
+
+import ItteriaGit.src.fill.fill22;
+
+public class Principal {
+
+	public static void main(String[] args) {
+
+		fill22 f22 = new fill22("Francisco Gienini");
+
+	}
+
+}
